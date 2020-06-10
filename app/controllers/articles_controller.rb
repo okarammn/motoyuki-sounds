3065a5e5fb47add169a5358653e9a5eb19bdec62
EC2 Default User <ec2-user@ip-172-31-46-129.ap-northeast-1.compute.internal>
2020-06-10 00:41:15 +0000

    new createアクション定義

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,20 @@
 class ArticlesController < ApplicationController
+  
+  def index
+    @articles = Article.all
+  end
+  
+  def new
+    @article = Article.new
+  end
+
+  def create
+    Articre.create(title: article_params[:title], text: article_params[:text], image: article_params[:image], user_id: current_user.id)
+  end
+  
+  private
+  
+  def article_params
+    paramas.require(:article).permit(:title, :text, :image)
+  end
 end
