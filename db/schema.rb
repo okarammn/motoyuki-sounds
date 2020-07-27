7729d58a3ec566374d4a0bf1595cd3bfc3198887
EC2 Default User <ec2-user@ip-172-31-46-129.ap-northeast-1.compute.internal>
2020-07-27 21:01:40 +0000

    rails g resouece contactを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_203636) do
+ActiveRecord::Schema.define(version: 2020_07_27_205244) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -39,6 +39,11 @@ ActiveRecord::Schema.define(version: 2020_07_27_203636) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["id"], name: "id", unique: true
+  end
+
+  create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
