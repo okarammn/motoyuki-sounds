a4b94838ae354288c32233cc1b7b30b226ec92d1
okarammn <mumin649651@gmail.com>
2020-08-04 05:01:34 +0000

    profilesコントローラー編集

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,28 @@
 class ProfilesController < ApplicationController
+  
+  before_action :move_to_top, except: [:show]
+  
+  def show
+    @profile = Profile.find(1)
+  end
+  
+  def edit
+    @profile = Profile.find(1)
+  end
+  
+  def update
+    profile = Profile.find(1)
+    profile.update(profile_params)
+  end
+  
+  private
+  
+  def profile_params
+    params.require(:profile).permit(:title, :text, :mini_text, :image)
+  end
+  
+  def move_to_top
+    redirect_to controller: :articles, action: :index unless user_signed_in?
+  end 
+    
 end
