f0e958a5b6430aa318c2d10ebe61a97bb5ea06f7
okarammn <mumin649651@gmail.com>
2020-08-05 19:53:39 +0000

    seeds.rb編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# Article.create(title: 'シード', text: 'うまくいくかな')  local2020:8/6 
+
+Profile.create(name: 'Motoyuki', text: 'test', mini_text: 'test2')
